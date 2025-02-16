bac59e7b979d952c9165f3341116855e605801fd
Fernando Filho <contato.fernandofilho1@gmail.com>
2025-02-16 11:16:49 -0300

    upadte code, final code

--- src/opensource/desafio/java/ContaTerminal.java
+++ src/opensource/desafio/java/ContaTerminal.java
@@ -1,27 +1,34 @@
 package opensource.desafio.java;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        int numeroDaContaBancaria = 1021;
-        int agenciaBancaria = 607-9;
-        String nomeCliente = "Mario Andrade";
-        double saldo = 237.48;
+        //TODO:"CONHECER e importar a classe Scanner"
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        //Exibir a mensagem da conta criada
 
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Programa: Por favor, digite o número da Agência !");
-        int numeroAgencia = entrada.nextInt();
-        System.out.println("Usuário: " + numeroAgencia + "(Pressione enter para prosseguir)");
-        System.out.println("Digite o número da agência!");
-        int agenciaBanco = entrada.nextInt();
-        System.out.println("Sua agência é: " + agenciaBanco + "(Pressione enter para prosseguir)" );
-        System.out.println("Digite seu nome e sobrenome:");
-        String nomeSobrenomeCliente = entrada.next();
-        System.out.println("Seu nome e sobrenome é: " + nomeSobrenomeCliente + "(Pressione enter para prosseguir)");
+
+        System.out.println("Digite a agência bancária !");
+        String agencia = entrada.nextLine();
+
+        System.out.println("Agora, digite o número da Agência !");
+        int numero = entrada.nextInt();
+
+
+        entrada.nextLine();
+
+        System.out.println("Digite seu nome e sobrenome: ");
+        String nomeDoCliente = entrada.nextLine();
+
         System.out.println("Digite seu saldo atual: ");
         double saldoClienteBanco = entrada.nextDouble();
-        System.out.println("Seu saldo atual é de: " + saldoClienteBanco + "(Pressione enter para prosseguir)");
-        System.out.println("Questionário finalizado !");
+
+        System.out.println("Olá nobre, " + nomeDoCliente + ", agradecemos por escolher o melhor e maior banco, " + " sua agência é " + agencia + ", conta " + numero + " e seu saldo atual é de " + saldoClienteBanco + " e já está disponível para o saque! " + " Agradecemos por a escolha.");
+
+        System.out.println("Conta criada com sucesso!");
+
+        entrada.close();
     }
 }
